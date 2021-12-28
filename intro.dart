822586b5d3dcdaa3d7e367f006731f77b6d9a310
ulrichgero <49178952+ulrichgero@users.noreply.github.com>
2021-12-28 18:40:00 +0000

    classes
    Adding classes constructor

--- intro.dart
+++ intro.dart
@@ -16,14 +16,25 @@ void main() {
   //family.forEach((k,v) => print ('$k:$v'));
 
   var emperor = User(1977, 'Palpatine', 'Naboo');
-  print(emperor.name);
-}
+  print(
+      'The ruler of the galaxy name is ${emperor.name.toUpperCase()} and he\'s from ${emperor.placeOgBirth.toUpperCase()}');
+  var skywalker = User(1977, 'Darth Vader', 'Tatooine');
+  print(
+      'Emperor ${emperor.name} new apprentice \nis going to rule the Galaxy and his name is ${skywalker.name.toUpperCase()}');
+
+  if (emperor.dateOfBirth < 1977) {
+    print('Error! You\'re not ${emperor.name}');
+  }else if (emperor.dateOfBirth == 1977) {
+    print('Welcome to the Dark Side');
+  }
+
  
+}
 
 // working with classes in Dart
 class User {
-  int dateOfBirth = 0; var name ='', placeOgBirth ='';
+  int dateOfBirth = 0;
+  var name = '', placeOgBirth = '';
   //This is the constructor
   User(this.dateOfBirth, this.name, this.placeOgBirth);
-  
 }
